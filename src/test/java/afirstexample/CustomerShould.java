593ce573b7a0cf7490da1b9e2db5153059ac5891
Alvaro Garcia <derecerca@gmail.com>
2015-03-31 05:25:31 +0200

    green: the customer should owe three (3.0) when she rented a release for a day, earning one frequent renter point

--- src/test/java/afirstexample/CustomerShould.java
+++ src/test/java/afirstexample/CustomerShould.java
@@ -7,10 +7,20 @@ import org.junit.Test;
 
 public class CustomerShould {
 
+	private static final Movie RELEASE_MOVIE = new Movie("release", Movie.NEW_RELEASE);
+
 	@Test
 	public void owe_zero_when_no_rentals() throws Exception {
 		final Customer customer = new Customer("John");
 		assertThat(customer.statement(), is("Rental Record for John\nAmount owed is 0.0\nYou earned 0 frequent renter points"));
+	}
+
+	@Test
+	public void owe_3_when_rented_a_release_for_a_day() throws Exception {
+		final Customer customer = new Customer("Alice");
+		customer.addRental(new Rental(RELEASE_MOVIE, 1));
+		assertThat(customer.statement(),
+				is("Rental Record for Alice\n\trelease\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points"));
 	}
 
 }
