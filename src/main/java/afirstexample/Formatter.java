f356f775f5f03cc2bc666fbd84f5e67ed3622a48
alvaro garcia <derecerca@gmail.com>
2015-04-11 23:00:50 +0200

    make constants follow the java convention

--- src/main/java/afirstexample/Formatter.java
+++ src/main/java/afirstexample/Formatter.java
@@ -4,6 +4,8 @@ package afirstexample;
  * Created by alvaro on 11/04/15.
  */
 public class Formatter {
+	private final String FIELD_SEPARATOR = "\t";
+	private final String NEWLINE_SEPARATOR = "\n";
 
 	public String formatStatement (final String customerName, final String figures, final String amountOwed, final String earnedRenterPoints) {
 		String result = "Rental Record for " + customerName + "\n";
@@ -14,13 +16,11 @@ public class Formatter {
 	}
 
 	public String formatFigure (final Rental rental, final double rentalAmount) {
-		final String fieldSeparator = "\t";
-		final String newLineSeparator = "\n";
-		return fieldSeparator + rental.getMovie().getTitle() + fieldSeparator + String.valueOf(rentalAmount) + newLineSeparator;
+		return FIELD_SEPARATOR + rental.getMovie().getTitle() + FIELD_SEPARATOR + String.valueOf(rentalAmount) + NEWLINE_SEPARATOR;
 	}
 
 	public String formatAmountOwed (final double amountOwed) {
-		return "Amount owed is " + String.valueOf(amountOwed)+"\n";
+		return "Amount owed is " + String.valueOf(amountOwed)+ NEWLINE_SEPARATOR;
 	}
 
 	public String formatRenterPoints (final int renterPoints) {
